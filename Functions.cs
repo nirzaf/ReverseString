edf565d7cf2043ddb7661d2c71cefa0fa64a6475
nirzaf <mfmfazrin1986@gmail.com>
2021-12-28 10:22:59 +0530

    Add project files.

---
+++ Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal static class Functions
+    {
+        public stringReverseString1(string str) {  
+            char[] chars = str.ToCharArray();  
+            char[] result = newchar[chars.Length];  
+            for (int i = 0, j = str.Length - 1; i < str.Length; i++, j--) {  
+                result[i] = chars[j];  
+            }  
+            return new string(result);  
+        }  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+public static class MyClass
+{
+    public static stringReverseString2(string str) {  
+        char[] chars = str.ToCharArray();  
+        for (int i = 0, j = str.Length - 1; i < j; i++, j--) {  
+            char c = chars[i];  
+            chars[i] = chars[j];  
+            chars[j] = c;  
+        }  
+        return new string(chars);  
+    } 
+}
